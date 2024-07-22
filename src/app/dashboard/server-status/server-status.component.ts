59aaaeaaa57cd8e241cec45f8e8389e6d7216b4f
Savchuk Volodymyr <savchuk.v03@gmail.com>
2024-07-22 11:42:45 +0300

    add server status emulation

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  ngOnInit() {
+    const interval = setInterval(() => {
+      const random = Math.random();
+
+      if (random < 0.5) {
+        this.currentStatus = 'online';
+      } else if (random < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
+  }
 }
